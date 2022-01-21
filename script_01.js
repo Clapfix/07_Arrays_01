d7ca2323b5724814b40214a838ebb0a75b7fa289
Clapfix <ronny.perner@kabelmail.de>
2022-01-21 08:56:43 +0100

    01. Funktionalität mit Einzelparametern

--- script_01.js
+++ script_01.js
@@ -3,11 +3,27 @@
 /* Die Satzbau-Maschine | Arrays */
 
 
+/** 01. Funktionalität mit Einzelparametern  */
+// --> "Ich bin Max Mütze."
+
+output(getSentence("Ich","bin","Max","Mütze"));
+output(getSentence("Ich","bin","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4) {
+
+    const gap  = " ";
+    const pnct = ".";
+    const str  =    word1 + gap + 
+                    word2 + gap +
+                    word3 + gap +
+                    word4 + 
+                    pnct;
+    return str;
+}
 
 
 
 // Modul: Ausgabe in Konsole : Test
-// output("hi"); 
+//output("hi"); 
 function output(outputStr) {
     console.log(outputStr);
 }
